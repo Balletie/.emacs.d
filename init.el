b9e22607371a9206a31666c42c35d14167fb46e6
Balletie <skip_meesie@hotmail.com>
2016-11-09 00:15:19 +0100

    Avy and popwin config

--- init.el
+++ init.el
@@ -347,3 +347,13 @@
 	battery-mode-line-limit 1000
 	battery-update-interval 5
 	sml/battery-format " %p%% "))
+
+(use-package avy
+  :bind (("C-," . avy-goto-char-2)
+	 ("C-;" . avy-goto-word-1)
+	 ("C-'" . avy-isearch)))
+
+(use-package popwin
+  :init
+  (require 'popwin)
+  (popwin-mode 1))
