31d775b5bba062958274fa3e4f6fc0943eff11e6
Taan Skip <skippetie@gmail.com>
2021-06-11 18:45:04 -0700

    Update custom.el vars

--- etc/custom.el
+++ etc/custom.el
@@ -3,10 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-want-C-u-scroll t))
+ '(evil-want-C-u-scroll t)
+ '(safe-local-variable-values
+   (quote
+    ((org-export-allow-bind-keywords . t)
+     (org-latex-inputenc-alist
+      ("utf8" . "utf8x"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
